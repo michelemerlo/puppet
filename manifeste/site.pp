3d13e4e00928eba20d88b4527ec52cc3e2c5ddd1
michelemerlo <michele.merlo@bl.ch>
2017-02-02 08:36:45 +0100

    Initial Commit

--- manifeste/site.pp
+++ manifeste/site.pp
@@ -2,4 +2,18 @@ node default { }
 
 node 'puppet.bl.ch' {
 include lamp
+notify { 'Testausgabe von Puppet erfolgreich. :)':
+}
+}
+node 'lp1.bl.ch' {
+include update
+notify { 'Testausgabe erfolgreich. :)':
+}
+}
+node 'wp01.bl.ch' {
+file { 'c:\\Temp\puppettestfile3.txt': # Resource type file
+ ensure => 'present', # Stellt sich dass es existiert
+ owner => 'Test', # Besitzer
+ content => "Dies ist das Testdokument3 zur Übung mit Puppet. 01.02.2017 Michèle Merlo ZI"
+}
 }
